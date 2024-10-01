458e934e20228b6a2754347c214d1463c017d05a
Vladimir Shkodin <v.s.shkodin@gmail.com>
2024-10-01 17:44:14 +0300

    Fixing src/WebApp.java for finding 1

--- src/WebApp.java
+++ src/WebApp.java
@@ -6,14 +6,16 @@ import javax.servlet.http.HttpServletResponse;
 
 public class WebApp extends HttpServlet {
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String userInput = req.getParameter("input");
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	    String userInput = req.getParameter("input");
 
-        // XSS vulnerability
-        resp.getWriter().println("<html><body>");
-        resp.getWriter().println("<h1>Welcome</h1>");
-        resp.getWriter().println("<p>You entered: " + userInput + "</p>"); // Potential XSS vulnerability
-        resp.getWriter().println("</body></html>");
-    }
+	    // XSS vulnerability
+	    resp.getWriter().println("<html><body>");
+	    resp.getWriter().println("<h1>Welcome</h1>");
+	    resp.getWriter().println("<p>You entered: " + HtmlUtils.htmlEscape(userInput) + "</p>"); // Fixed XSS vulnerability
+	    resp.getWriter().println("</body></html>");
+	}
+
 }
+
